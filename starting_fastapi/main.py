1e85ee47e49f99364c9ff98ee3362474b11c9978
Muhammad_Hammad_Shah <Hammad4455shah5544@gmail.com>
2024-06-16 22:47:00 +0500

    alot of work was done again.....But more will also come.

--- starting_fastapi/main.py
+++ starting_fastapi/main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,16 +12,40 @@ class Post(BaseModel):
     published : bool = True # True is set as default
     rating : Optional[int] = None
 
+
+
+my_db_my_posts = [{
+    "title" : "title of post 1",
+    "content" : "content of post 1",
+    "id" : 1
+},
+{
+    "title" : "favourite foods",
+    "content" : "I like pizza",
+    "id" : 2
+}] # this is a self made database for simple use
+
+#function to match the id's in our own db my_db_my_posts
+def find_posts_id(id):
+    for p in my_db_my_posts:
+        if p["id"] == id:
+            return p
+
+
 @app.get("/")
 async def root():
     return {
         "message": "Hello to this  World"
     }
+
+
 @app.get("/posts")
-def root():
+def get_posts_get_param():
     return {
-        "message": "This is your posts"
-    }
+        "data": my_db_my_posts
+        }
+        #"message_for_you": "This is your posts" >>>> this one was before but now we will get data from db
+    
     
 @app.post("/first_create_posts")
 def first_creates_post(payload:dict = Body(...)): #Body is an object from fastapi.params and which geets data from body as we saw in postman 
@@ -32,9 +57,26 @@ def first_creates_post(payload:dict = Body(...)): #Body is an object from fastap
     }          
 
 @app.post("/createposts")                     
-def create_posts(new_post : Post):
-    print(new_post)
+def create_posts(new_post : Post): # we can make this post request to get requests by send the param new_post in return 
+    print(new_post)# to add some data our db my_db_my_posts but we have to give random numbers to id's for our db so we will also use randomrange packages
+    new_post_dict = new_post.dict()
+    new_post_dict["id"] = randrange(0,1000000000000)
+    my_db_my_posts.append(new_post_dict)
+
+    #print(new_post.dict()) # .dict() can make values into dictionary again.
     return{
-        "data" : "your send new data is correct and working fine."
+        "data" : new_post
     }
     
+
+# Get a single post with id
+
+@app.get("/posts/{id}") # when ever we get a value from path it becomes string
+def get_a_single_post(id):
+    specific_post_id = find_posts_id(int(id)) # So we will turn it into integer 
+    #print(id)
+    print(specific_post_id)
+    return{
+        #"details" : f"Here is posts_id >>>> {id} <<<< you are interested in "
+          "details" :  specific_post_id
+    }
