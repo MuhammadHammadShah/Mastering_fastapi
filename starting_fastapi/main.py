c290613d291807ef8d5f4a3e7a88f52b1cace594
Muhammad_Hammad_Shah <Hammad4455shah5544@gmail.com>
2024-06-19 12:19:26 +0500

    created a PUT method

--- starting_fastapi/main.py
+++ starting_fastapi/main.py
@@ -106,3 +106,14 @@ def delete(id : int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail=f"post with id: {id} not found")
     my_db_my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# To update a method with PUT
+@app.put("/posts/{id}")
+def update_post_put(id:int , post : Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail=f"post with id: {id} not found")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_db_my_posts[index] = post_dict
+    return {"data" : post_dict}
